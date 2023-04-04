1e0f365e84a1d4825ef3f997ddf307608abf9a70
Derek_Devs <derek@derekdevs.com>
2023-04-03 20:27:02 -0500

    Added many features!

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -6,18 +6,19 @@
         themeChange(false)
 })
 </script>
-  <div class="navbar bg-base-100">
+<div class="sticky top-0">
+  <div class="navbar bg-hero-pattern bg-secondary rounded-sm bg-transparent">
     <div class="navbar-start">
       <div class="dropdown">
         <!-- svelte-ignore a11y-no-noninteractive-tabindex -->
         <!-- svelte-ignore a11y-no-noninteractive-tabindex -->
         <!-- svelte-ignore a11y-label-has-associated-control -->
-        <label tabindex="0" class="btn btn-ghost btn-circle">
+        <label tabindex="0" class="btn btn-square">
           <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h16M4 18h7" /></svg>
         </label>
         <!-- svelte-ignore a11y-missing-attribute -->
         <!-- svelte-ignore a11y-no-noninteractive-tabindex -->
-        <ul tabindex="0" class="menu menu-compact dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52">
+        <ul tabindex="0" class="menu menu-compact dropdown-content mt-3 p-2 shadow from-primary rounded-box w-52">
           <!-- svelte-ignore a11y-missing-attribute -->
           <li><a>Homepage</a></li>
           <li><a>Portfolio</a></li>
@@ -27,12 +28,9 @@
     </div>
     <div class="navbar-center">
       <!-- svelte-ignore a11y-missing-attribute -->
-      <a class="btn btn-ghost normal-case text-xl">Glitch Breakers</a>
+      <a class="btn normal-case text-xl">Glitch Breakers</a>
     </div>
     <div class="navbar-end">
-      <button class="btn btn-ghost btn-circle">
-        <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z" /></svg>
-      </button>
       <select data-choose-theme>
         <option value="night">Default</option>
         <option value="acid">Light</option>
@@ -40,4 +38,5 @@
       </select>
     </div>
   </div>
+</div>
   <slot />
---
+++ src/routes/+page.svelte
@@ -0,0 +1,42 @@
+<script>
+
+</script>
+
+<div class="flex flex-col w-full lg:flex-row">
+
+<div class="hero min-h-screen rounded-box" style="background-image: url(/static/img1.png);">
+    <div class="hero-overlay bg-opacity-60"></div>
+    <div class="hero-content text-center text-neutral-content">
+      <div class="max-w-md">
+        <h1 class="mb-5 text-5xl font-bold">Welcome to Glitch Breakers!</h1>
+        <p class="mb-5">Our dedicated team of expert developers, designers, and strategists work collaboratively to create bespoke web solutions tailored to your unique needs. We blend cutting-edge technologies with innovative strategies to design custom web applications that not only look stunning but also deliver outstanding performance.</p>
+        <button class="btn btn-primary">Get Started</button>
+      </div>
+    </div>
+  </div>
+
+  <div class="divider lg:divider-horizontal"></div> 
+
+  <div class="hero min-h-screen rounded-box" style="background-image: url(/static/img2.png);">
+    <div class="hero-overlay bg-opacity-60"></div>
+    <div class="hero-content text-center text-neutral-content">
+      <div class="max-w-md">
+        <h1 class="mb-5 text-5xl font-bold">Why choose Glitch Breakers?</h1>
+        <p class="mb-5">
+        <ul>
+            <li>🔹 Unwavering Reliability: We ensure your web applications are rock solid and built to withstand the test of time.</li>
+            <div class="divider"></div> 
+            <li>🔹 Scalability for Growth: Our solutions are designed to adapt and expand as your business grows, ensuring long-term success.</li>
+            <div class="divider"></div> 
+            <li>🔹 Impactful Results: We focus on delivering web applications that drive tangible results, increasing efficiency and maximizing your return on investment.</li>
+            <div class="divider"></div> 
+            <li>🔹 Exceptional Support: Our team is committed to providing unparalleled support, from the initial concept to post-launch maintenance and beyond.</li>
+            <div class="divider"></div> 
+
+        </ul>      
+        <button class="btn btn-primary">Get Started</button>
+      </div>
+    </div>
+  </div>
+
+</div>
