47223de2e8b9e992a7724fb5a5707c9ad991bf52
Derek_Devs <derek@derekdevs.com>
2023-04-19 17:17:14 -0500

    Production Test 0.0.3

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -9,22 +9,17 @@
 <div class="sticky top-0">
   <div class="navbar bg-hero-pattern bg-secondary rounded-sm bg-transparent">
     <div class="navbar-start">
-      <div class="dropdown">
-        <!-- svelte-ignore a11y-no-noninteractive-tabindex -->
-        <!-- svelte-ignore a11y-no-noninteractive-tabindex -->
-        <!-- svelte-ignore a11y-label-has-associated-control -->
+      <!-- Still working on this dropdown -->
+      <!-- <div class="dropdown">
         <label tabindex="0" class="btn btn-square">
           <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h16M4 18h7" /></svg>
         </label>
-        <!-- svelte-ignore a11y-missing-attribute -->
-        <!-- svelte-ignore a11y-no-noninteractive-tabindex -->
         <ul tabindex="0" class="menu menu-compact dropdown-content mt-3 p-2 shadow from-primary rounded-box w-52">
-          <!-- svelte-ignore a11y-missing-attribute -->
           <li><a href="/">Home</a></li>
           <li><a href="/testimonials">Testimonials</a></li>
           <li><a href="/about">About</a></li>
         </ul>
-      </div>
+      </div> -->
     </div>
     <div class="navbar-center">
       <!-- svelte-ignore a11y-missing-attribute -->
